52627f1893c6ff0a728caec3dbc1019dba1175a3
jack9603301 <jack9603301@163.com>
2020-09-30 15:56:08 +0800

    fix: Fix detected bugs and project settings

--- WP-ReliableMD.php
+++ WP-ReliableMD.php
@@ -24,6 +24,6 @@ define('WPReliableMD_PATH', dirname( __FILE__ ) ); //插件路径文件夹
 //自动加载
 require_once WPReliableMD_PATH . '/vendor/autoload.php';
 
-new Main();  //进入插件主处理程序
+$main = new Main();  //进入插件主处理程序
 
 ?>
